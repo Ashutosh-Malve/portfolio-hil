c1671ebe314aec752164e72784654977a1a57e51
Ashutosh Malve <ashutoshmalve@hotmail.com>
2023-09-12 08:35:16 +0530

    hcq added page

--- components/sections/index/looking.jsx
+++ components/sections/index/looking.jsx
@@ -1,6 +1,7 @@
 // Section structure
 import Section 		from '../../structure/section';
 import Container 	from '../../structure/container';
+import button 		from '../../../styles/blocks/button.module.scss';
 
 // Specing util
 import Spacing 		from '../../utils/spacing.util';
@@ -25,15 +26,20 @@ export default function Looking() {
 			<Container classProp={`${section.title} ${looking.container}`} spacing={['verticalXXXLrg']}>
 				
 			<h4> </h4>
-				 <h2><span className={looking.highlight2}>Code</span> <span className={looking.highlight}>For</span>  <span className={looking.highlight}>Cause</span>  Hackathon</h2> 
+				 <h2><span className={looking.highlight2}>Harmony</span> <span className={looking.highlight}>Code</span>  <span className={looking.highlight}>Quest</span>  2023</h2> 
 				 <p className="subtitle">The &quot;Harmony Code Quest&quot; is not just a hackathon; it&apos;s a call to arms for the bright minds of today to use their skills and passion to create solutions that resonate with the essence of the Heartfulness Innovation Lab. Together, let us forge a path where technology serves as a bridge, connecting hearts and minds for a more compassionate and enlightened future.</p> 
 				 
-				<h2 className={looking.json}>motivation : &#123;</h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight}>Empowering Minds</span>,</h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight2}>Enriching Lives</span>,</h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight}> Enhancing Nature</span>,</h2>
+ 				<Container spacing={['verticalLrg']}> 
+			
+				 </Container>
+				<Container spacing={['verticalLrg']}> 	
+				 <button	className={`button ${button.primary} ` }
+							onClick={ () => window.location = 'https://lu.ma/rtpwaw7s' } >
+						{'Register Now'}
+					</button>			 
+				</Container>
 
-				<h2 className={looking.json}>&#125;</h2>
+
 			</Container>
 		</Section>
 	)
